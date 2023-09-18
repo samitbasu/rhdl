378b047cf8ea6ea55bd40c2f4555121e0acd57fd
Samit Basu <basu.samit@gmail.com>
2023-09-17 21:25:46 -0700

    add b and s types

--- rhdl-macro-core/src/digital.rs
+++ rhdl-macro-core/src/digital.rs
@@ -39,9 +39,27 @@ fn derive_digital_tuple_struct(decl: DeriveInput) -> anyhow::Result<TokenStream>
                 .enumerate()
                 .map(|(ndx, field)| syn::LitInt::new(&ndx.to_string(), field.span()));
             let fields2 = fields.clone();
+            let fields_3 = fields.clone();
+            let fields_4 = fields.clone();
             let field_types = s.fields.iter().map(|x| &x.ty);
+            let field_types2 = field_types.clone();
+            let field_types_3 = field_types.clone();
             Ok(quote! {
                 impl #impl_generics rhdl_core::Digital for #struct_name #ty_generics #where_clause {
+                    fn static_kind() -> rhdl_core::Kind {
+                        rhdl_core::Kind::make_struct(vec![
+                            #(
+                                rhdl_core::Kind::make_field(stringify!(#fields_4), <#field_types_3 as rhdl_core::Digital>::static_kind()),
+                            )*
+                        ])
+                    }
+                    fn bin(self) -> Vec<bool> {
+                        let mut result = vec![];
+                        #(
+                            result.extend(self.#fields_3.bin());
+                        )*
+                        result
+                    }
                     fn allocate<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, builder: impl rhdl_core::LogBuilder) {
                         #(
                             <#field_types as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(#fields)));
@@ -50,6 +68,11 @@ fn derive_digital_tuple_struct(decl: DeriveInput) -> anyhow::Result<TokenStream>
                     fn record<L: rhdl_core::Digital>(&self, tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
                         #(
                             self.#fields2.record(tag, &mut logger);
+                        )*
+                    }
+                    fn skip<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
+                        #(
+                            <#field_types2 as rhdl_core::Digital>::skip(tag, &mut logger);
                         )*
                     }
                 }
@@ -66,9 +89,27 @@ fn derive_digital_named_struct(decl: DeriveInput) -> anyhow::Result<TokenStream>
         Data::Struct(s) => {
             let fields = s.fields.iter().map(|field| &field.ident);
             let fields2 = fields.clone();
+            let fields_3 = fields.clone();
+            let fields_4 = fields.clone();
             let field_types = s.fields.iter().map(|x| &x.ty);
+            let field_types_2 = field_types.clone();
+            let field_types_3 = field_types.clone();
             Ok(quote! {
                 impl #impl_generics rhdl_core::Digital for #struct_name #ty_generics #where_clause {
+                    fn static_kind() -> rhdl_core::Kind {
+                        rhdl_core::Kind::make_struct(vec![
+                            #(
+                                rhdl_core::Kind::make_field(stringify!(#fields_4), <#field_types_3 as rhdl_core::Digital>::static_kind()),
+                            )*
+                        ])
+                    }
+                    fn bin(self) -> Vec<bool> {
+                        let mut result = vec![];
+                        #(
+                            result.extend(self.#fields_3.bin());
+                        )*
+                        result
+                    }
                     fn allocate<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, builder: impl rhdl_core::LogBuilder) {
                         #(
                             <#field_types as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(#fields)));
@@ -77,6 +118,11 @@ fn derive_digital_named_struct(decl: DeriveInput) -> anyhow::Result<TokenStream>
                     fn record<L: rhdl_core::Digital>(&self, tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
                         #(
                             self.#fields2.record(tag, &mut logger);
+                        )*
+                    }
+                    fn skip<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
+                        #(
+                            <#field_types_2 as rhdl_core::Digital>::skip(tag, &mut logger);
                         )*
                     }
                 }
@@ -164,6 +210,20 @@ mod test {
         let output = derive_digital(decl).unwrap();
         let expected = quote! {
             impl rhdl_core::Digital for NestedBits {
+                fn static_kind() -> rhdl_core::Kind {
+                    rhdl_core::Kind::make_struct(vec![
+                        rhdl_core::Kind::make_field(stringify!(nest_1), <bool as rhdl_core::Digital>::static_kind()),
+                        rhdl_core::Kind::make_field(stringify!(nest_2), <u8 as rhdl_core::Digital>::static_kind()),
+                        rhdl_core::Kind::make_field(stringify!(nest_3), <TwoBits as rhdl_core::Digital>::static_kind()),
+                    ])
+                }
+                fn bin(self) -> Vec<bool> {
+                    let mut result = vec![];
+                    result.extend(self.nest_1.bin());
+                    result.extend(self.nest_2.bin());
+                    result.extend(self.nest_3.bin());
+                    result
+                }
                 fn allocate<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, builder: impl rhdl_core::LogBuilder) {
                     <bool as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(nest_1)));
                     <u8 as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(nest_2)));
@@ -173,6 +233,11 @@ mod test {
                     self.nest_1.record(tag, &mut logger);
                     self.nest_2.record(tag, &mut logger);
                     self.nest_3.record(tag, &mut logger);
+                }
+                fn skip<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
+                    <bool as rhdl_core::Digital>::skip(tag, &mut logger);
+                    <u8 as rhdl_core::Digital>::skip(tag, &mut logger);
+                    <TwoBits as rhdl_core::Digital>::skip(tag, &mut logger);
                 }
             }
         };
@@ -192,42 +257,37 @@ mod test {
         let expected = quote! {
             impl rhdl_core::Digital for Inputs {
                 fn static_kind() -> rhdl_core::Kind {
-                    rhdl_core::Kind::Struct {
-                        fields: vec![
-                            rhdl_core::Field {
-                                name: "input".to_string(),
-                                kind: <u32 as rhdl_core::Digital>::static_kind(),
-                            },
-                            rhdl_core::Field {
-                                name: "write".to_string(),
-                                kind: <bool as rhdl_core::Digital>::static_kind(),
-                            },
-                            rhdl_core::Field {
-                                name: "read".to_string(),
-                                kind: <bool as rhdl_core::Digital>::static_kind(),
-                            },
-                        ],
-                    }
+                    rhdl_core::Kind::make_struct(vec![
+                        rhdl_core::Kind::make_field(stringify!(input), <u32 as rhdl_core::Digital>::static_kind()),
+                        rhdl_core::Kind::make_field(stringify!(write), <bool as rhdl_core::Digital>::static_kind()),
+                        rhdl_core::Kind::make_field(stringify!(read), <bool as rhdl_core::Digital>::static_kind()),
+                    ])
                 }
                 fn bin(self) -> Vec<bool> {
-                    let mut v = self.input.bin();
-                    v.extend(self.write.bin());
-                    v.extend(self.read.bin());
-                    v
+                    let mut result = vec![];
+                    result.extend(self.input.bin());
+                    result.extend(self.write.bin());
+                    result.extend(self.read.bin());
+                    result
                 }
                 fn allocate<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, builder: impl rhdl_core::LogBuilder) {
                     <u32 as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(input)));
                     <bool as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(write)));
                     <bool as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(read)));
                 }
-
                 fn record<L: rhdl_core::Digital>(&self, tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
                     self.input.record(tag, &mut logger);
                     self.write.record(tag, &mut logger);
                     self.read.record(tag, &mut logger);
                 }
+                fn skip<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
+                    <u32 as rhdl_core::Digital>::skip(tag, &mut logger);
+                    <bool as rhdl_core::Digital>::skip(tag, &mut logger);
+                    <bool as rhdl_core::Digital>::skip(tag, &mut logger);
+                }
             }
         };
+
         assert_tokens_eq(&expected, &output);
     }
 
@@ -244,39 +304,33 @@ mod test {
         let expected = quote! {
             impl rhdl_core::Digital for Inputs {
                 fn static_kind() -> rhdl_core::Kind {
-                    rhdl_core::Kind::Struct {
-                        fields: vec![
-                            rhdl_core::Field {
-                                name: "input".to_string(),
-                                kind: <u32 as rhdl_core::Digital>::static_kind(),
-                            },
-                            rhdl_core::Field {
-                                name: "write".to_string(),
-                                kind: <bool as rhdl_core::Digital>::static_kind(),
-                            },
-                            rhdl_core::Field {
-                                name: "read".to_string(),
-                                kind: <(bool, bool) as rhdl_core::Digital>::static_kind(),
-                            },
-                        ],
-                    }
+                    rhdl_core::Kind::make_struct(vec![
+                        rhdl_core::Kind::make_field(stringify!(input), <u32 as rhdl_core::Digital>::static_kind()),
+                        rhdl_core::Kind::make_field(stringify!(write), <bool as rhdl_core::Digital>::static_kind()),
+                        rhdl_core::Kind::make_field(stringify!(read), <(bool, bool) as rhdl_core::Digital>::static_kind()),
+                    ])
                 }
                 fn bin(self) -> Vec<bool> {
-                    let mut v = self.input.bin();
-                    v.extend(self.write.bin());
-                    v.extend(self.read.bin());
-                    v
+                    let mut result = vec![];
+                    result.extend(self.input.bin());
+                    result.extend(self.write.bin());
+                    result.extend(self.read.bin());
+                    result
                 }
                 fn allocate<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, builder: impl rhdl_core::LogBuilder) {
                     <u32 as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(input)));
                     <bool as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(write)));
                     <(bool, bool) as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(read)));
                 }
-
                 fn record<L: rhdl_core::Digital>(&self, tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
                     self.input.record(tag, &mut logger);
                     self.write.record(tag, &mut logger);
                     self.read.record(tag, &mut logger);
+                }
+                fn skip<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
+                    <u32 as rhdl_core::Digital>::skip(tag, &mut logger);
+                    <bool as rhdl_core::Digital>::skip(tag, &mut logger);
+                    <(bool, bool) as rhdl_core::Digital>::skip(tag, &mut logger);
                 }
             }
         };
@@ -292,39 +346,33 @@ mod test {
         let expected = quote! {
             impl rhdl_core::Digital for Inputs {
                 fn static_kind() -> rhdl_core::Kind {
-                    rhdl_core::Kind::Struct {
-                        fields: vec![
-                            rhdl_core::Field {
-                                name: "0".to_string(),
-                                kind: <u32 as rhdl_core::Digital>::static_kind(),
-                            },
-                            rhdl_core::Field {
-                                name: "1".to_string(),
-                                kind: <bool as rhdl_core::Digital>::static_kind(),
-                            },
-                            rhdl_core::Field {
-                                name: "2".to_string(),
-                                kind: <bool as rhdl_core::Digital>::static_kind(),
-                            },
-                        ],
-                    }
+                    rhdl_core::Kind::make_struct(vec![
+                        rhdl_core::Kind::make_field(stringify!(0), <u32 as rhdl_core::Digital>::static_kind()),
+                        rhdl_core::Kind::make_field(stringify!(1), <bool as rhdl_core::Digital>::static_kind()),
+                        rhdl_core::Kind::make_field(stringify!(2), <bool as rhdl_core::Digital>::static_kind()),
+                    ])
                 }
-                fn bin() -> Vec<bool> {
-                    let mut v = self.0.bin();
-                    v.extend(self.1.bin());
-                    v.extend(self.2.bin());
-                    v
+                fn bin(self) -> Vec<bool> {
+                    let mut result = vec![];
+                    result.extend(self.0.bin());
+                    result.extend(self.1.bin());
+                    result.extend(self.2.bin());
+                    result
                 }
                 fn allocate<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, builder: impl rhdl_core::LogBuilder) {
                     <u32 as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(0)));
                     <bool as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(1)));
                     <bool as rhdl_core::Digital>::allocate(tag, builder.namespace(stringify!(2)));
                 }
-
                 fn record<L: rhdl_core::Digital>(&self, tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
                     self.0.record(tag, &mut logger);
                     self.1.record(tag, &mut logger);
                     self.2.record(tag, &mut logger);
+                }
+                fn skip<L: rhdl_core::Digital>(tag: rhdl_core::TagID<L>, mut logger: impl rhdl_core::LoggerImpl) {
+                    <u32 as rhdl_core::Digital>::skip(tag, &mut logger);
+                    <bool as rhdl_core::Digital>::skip(tag, &mut logger);
+                    <bool as rhdl_core::Digital>::skip(tag, &mut logger);
                 }
             }
         };
