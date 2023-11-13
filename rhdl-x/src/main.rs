8125a5e1a2e5156631cc693daddd93f87ef7f64f
Samit Basu <basu.samit@gmail.com>
2023-11-13 08:47:43 -0800

    Aha.

--- rhdl-x/src/main.rs
+++ rhdl-x/src/main.rs
@@ -3,11 +3,49 @@ use anyhow::{anyhow, bail};
 use std::collections::{BTreeMap, HashSet};
 use std::env::args;
 use std::path::Path;
-use std::vec;
 use std::{collections::HashMap, fmt::Display};
+use std::{default, vec};
 use syn::token::In;
 use utils::IndentingFormatter;
 
 pub mod utils;
 
-fn main() {}
+trait Hello {
+    fn hello(&self) {
+        println!("Hello, world!");
+    }
+}
+
+#[derive(Default)]
+struct Foo {
+    a: i32,
+    b: i32,
+}
+
+impl Hello for Foo {}
+
+#[derive(Default)]
+enum Color {
+    #[default]
+    Red,
+    Green(u8),
+    Blue {
+        a: i32,
+        b: i32,
+    },
+}
+
+impl Hello for Color {}
+
+fn main() {
+    // Check for default via variant syntax
+    let a = Color::Red; // a is Color::Red
+    a.hello();
+    let b = Color::Green(Default::default());
+    b.hello();
+    let c = Color::Blue {
+        a: Default::default(),
+        b: Default::default(),
+    };
+    c.hello();
+}
