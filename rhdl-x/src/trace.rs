74711ad0c6085fac476d0e63fd50533037e7fc1a
Samit Basu <basu.samit@gmail.com>
2024-02-26 20:06:53 -0800

    Still working on trace.

--- rhdl-x/src/trace.rs
+++ rhdl-x/src/trace.rs
@@ -11,23 +11,28 @@ use rhdl_core::{
 pub fn trace(dfg: &DFG, node: NodeIndex<u32>, path: &Path) -> Result<()> {
     // First, iterate through the edges that end on node
     // and find the one that covers the given path.
-    let component = dfg.graph[node];
+    let component = &dfg.graph[node];
     let Some(covering_edge) = dfg
         .graph
         .edges_directed(node, Direction::Incoming)
         .find(|edge| {
-            path_covers(component.input.clone(), &edge.weight().dest, path).unwrap_or_default()
+            path_covers(component.input.clone(), path, &edge.weight().dest).unwrap_or_default()
         })
     else {
         bail!("No edge covers the given path");
     };
     let link = covering_edge.weight();
     let path = path_transmute(path, &link.dest, &link.src);
+    eprintln!("Path: {}", path);
+    Ok(())
 }
 
 fn path_covers(kind: Kind, sub_path: &Path, parent_path: &Path) -> Result<bool> {
+    eprintln!("path covers {} {} {}", kind, sub_path, parent_path);
     let sub_bit_range = bit_range(kind.clone(), &sub_path)?;
     let parent_bit_range = bit_range(kind, &parent_path)?;
+    eprintln!("sub_bit_range: {:?}", sub_bit_range);
+    eprintln!("parent_bit_range: {:?}", parent_bit_range);
     Ok(parent_bit_range.0.contains(&sub_bit_range.0.start)
         && parent_bit_range.0.contains(&sub_bit_range.0.end))
 }
@@ -35,10 +40,9 @@ fn path_covers(kind: Kind, sub_path: &Path, parent_path: &Path) -> Result<bool> 
 fn path_transmute(original: &Path, prefix_to_remove: &Path, prefix_to_add: &Path) -> Path {
     let path_with_prefix_stripped = original
         .iter()
-        .cloned()
         .skip(prefix_to_remove.len())
+        .cloned()
         .collect::<Path>();
     let mut new_path = prefix_to_add.clone();
-    new_path.join(&path_with_prefix_stripped);
-    new_path
+    new_path.join(&path_with_prefix_stripped)
 }
