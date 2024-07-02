b75cdb8379f12186bd6b68745125b69d0e91271b
Samit Basu <basu.samit@gmail.com>
2024-07-01 22:17:41 -0700

    Start clean up of circuits.

---
+++ rhdl-core/src/types/clock.rs
@@ -0,0 +1,34 @@
+use rand::Rng;
+
+use crate::{Digital, Kind, Notable, NoteKey, NoteWriter};
+
+#[derive(PartialEq, Clone, Copy, Debug)]
+pub struct Clock(bool);
+
+impl Clock {
+    pub fn raw(&self) -> bool {
+        self.0
+    }
+}
+
+pub fn clock(b: bool) -> Clock {
+    Clock(b)
+}
+
+impl Digital for Clock {
+    fn static_kind() -> Kind {
+        Kind::make_bool()
+    }
+    fn bin(self) -> Vec<bool> {
+        vec![self.0]
+    }
+    fn random() -> Self {
+        Clock(rand::thread_rng().gen::<bool>())
+    }
+}
+
+impl Notable for Clock {
+    fn note(&self, key: impl NoteKey, mut writer: impl NoteWriter) {
+        writer.write_bool(key, self.0);
+    }
+}
