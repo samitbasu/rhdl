be0feb8ab90d5dc5083f53e046d28b4bdf3dc0c2
Samit Basu <basu.samit@gmail.com>
2024-09-03 08:20:41 -0700

    Rework the symbols for RTL to eliminate the Slot references.

--- rhdl-core/src/rtl/symbols.rs
+++ rhdl-core/src/rtl/symbols.rs
@@ -1,4 +1,7 @@
-use std::collections::{BTreeMap, BTreeSet, HashMap};
+use std::{
+    collections::{BTreeMap, BTreeSet, HashMap},
+    ops::Range,
+};
 
 use crate::{
     ast::{ast_impl::FunctionId, spanned_source::SpannedSource},
@@ -15,3 +18,27 @@ pub struct SymbolMap {
     pub aliases: BTreeMap<Operand, BTreeSet<Operand>>,
 }
 
+impl SymbolMap {
+    pub fn source(&self) -> String {
+        self.sources
+            .values()
+            .fold(String::new(), |acc, src| acc + &src.source)
+    }
+    pub fn span(&self, loc: SourceLocation) -> Range<usize> {
+        let mut offset = 0;
+        for (id, src) in &self.sources {
+            if *id == loc.func {
+                let span = src.span(loc.node);
+                return (span.start + offset)..(span.end + offset);
+            }
+            offset += src.source.len();
+        }
+        panic!("SourceLocation not found in SymbolMap");
+    }
+    pub fn alias(&mut self, op: Operand, alias: Operand) {
+        self.aliases.entry(op).or_default().insert(alias);
+    }
+    pub fn fallback(&self, func: FunctionId) -> SourceLocation {
+        (func, self.sources[&func].fallback).into()
+    }
+}
