d858ed341e186ec814642f7d23d0d7b1aba8662e
Samit Basu <basu.samit@gmail.com>
2024-09-08 21:01:04 -0700

    Move check for zero-resize to end of optimization passes.

--- rhdl/tests/tuple.rs
+++ rhdl/tests/tuple.rs
@@ -13,7 +13,7 @@ mod common;
 use common::*;
 
 #[test]
-fn test_tuple_destructure_in_args() {
+fn test_tuple_destructure_in_args() -> miette::Result<()> {
     #[kernel]
     fn add((b, c): (Signal<u8, Red>, Signal<u8, Red>)) -> Signal<u8, Red> {
         b + c
@@ -22,11 +22,12 @@ fn test_tuple_destructure_in_args() {
     let test_vec = (0..4)
         .flat_map(|a| (0..4).map(move |b| ((red(a), red(b)),)))
         .collect::<Vec<_>>();
-    test_kernel_vm_and_verilog::<add, _, _, _>(add, test_vec.into_iter()).unwrap();
+    test_kernel_vm_and_verilog::<add, _, _, _>(add, test_vec.into_iter())?;
+    Ok(())
 }
 
 #[test]
-fn test_tuple_struct_nested_init() {
+fn test_tuple_struct_nested_init() -> miette::Result<()> {
     #[derive(PartialEq, Copy, Clone, Debug, Digital)]
     pub struct Foo {
         a: u8,
@@ -52,32 +53,35 @@ fn test_tuple_struct_nested_init() {
         signal(a + c + d)
     }
 
-    test_kernel_vm_and_verilog::<add<Red>, _, _, _>(add::<Red>, tuple_u8()).unwrap()
+    test_kernel_vm_and_verilog::<add<Red>, _, _, _>(add::<Red>, tuple_u8())?;
+    Ok(())
 }
 
 #[test]
-fn test_tuple_construct() {
+fn test_tuple_construct() -> miette::Result<()> {
     #[kernel]
     fn foo(a: Signal<b8, Red>, b: Signal<b8, Red>) -> (Signal<b8, Red>, Signal<b8, Red>) {
         (a, b)
     }
 
-    test_kernel_vm_and_verilog::<foo, _, _, _>(foo, tuple_pair_b8_red()).unwrap();
+    test_kernel_vm_and_verilog::<foo, _, _, _>(foo, tuple_pair_b8_red())?;
+    Ok(())
 }
 
 #[test]
-fn test_tuple_indexing() {
+fn test_tuple_indexing() -> miette::Result<()> {
     #[kernel]
     fn foo(a: Signal<b8, Red>, b: Signal<b8, Red>) -> Signal<b8, Red> {
         let c = (a, b);
         c.0 + c.1
     }
 
-    test_kernel_vm_and_verilog::<foo, _, _, _>(foo, tuple_pair_b8_red()).unwrap();
+    test_kernel_vm_and_verilog::<foo, _, _, _>(foo, tuple_pair_b8_red())?;
+    Ok(())
 }
 
 #[test]
-fn test_tuple_construct_and_deconstruct() {
+fn test_tuple_construct_and_deconstruct() -> miette::Result<()> {
     #[kernel]
     fn foo(a: Signal<b8, Red>, b: Signal<b8, Red>) -> Signal<b8, Red> {
         let c = (a, b);
@@ -85,41 +89,45 @@ fn test_tuple_construct_and_deconstruct() {
         d + e
     }
 
-    test_kernel_vm_and_verilog::<foo, _, _, _>(foo, tuple_pair_b8_red()).unwrap();
+    test_kernel_vm_and_verilog::<foo, _, _, _>(foo, tuple_pair_b8_red())?;
+    Ok(())
 }
 
 #[test]
-fn test_nested_tuple_indexing() {
+fn test_nested_tuple_indexing() -> miette::Result<()> {
     #[kernel]
     fn foo(a: Signal<b8, Red>, b: Signal<b8, Red>) -> Signal<b8, Red> {
         let c = (a, (b, a));
         c.1 .0 + c.1 .1
     }
 
-    test_kernel_vm_and_verilog::<foo, _, _, _>(foo, tuple_pair_b8_red()).unwrap();
+    test_kernel_vm_and_verilog::<foo, _, _, _>(foo, tuple_pair_b8_red())?;
+    Ok(())
 }
 
 #[test]
-fn test_nested_tuple_init() {
+fn test_nested_tuple_init() -> miette::Result<()> {
     #[kernel]
-    fn add<C: Domain>(a: Signal<u8, C>) -> Signal<u8, C> {
-        let b = (1, (2, 3), 4);
+    fn add<C: Domain>(a: Signal<b8, C>) -> Signal<b8, C> {
+        let b = (b8(1), (b8(2), b8(3)), b8(4));
         let (c, (d, e), f) = b;
         signal(c + d + e + f) + a
     }
 
-    test_kernel_vm_and_verilog::<add<Red>, _, _, _>(add::<Red>, tuple_u8()).unwrap();
+    test_kernel_vm_and_verilog::<add<Red>, _, _, _>(add::<Red>, tuple_exhaustive_red())?;
+    Ok(())
 }
 
 #[test]
-fn test_nested_tuple_array_init() {
+fn test_nested_tuple_array_init() -> miette::Result<()> {
     #[kernel]
-    fn add<C: Domain>(a: Signal<u8, C>) -> Signal<u8, C> {
-        let b = [(1, (2, 3), 4); 3];
+    fn add<C: Domain>(a: Signal<b8, C>) -> Signal<b8, C> {
+        let b = [(b8(1), (b8(2), b8(3)), b8(4)); 3];
         let (c, (d, e), f) = b[1];
         let [g, (h0, (h1a, h1b), h2), i] = b;
         signal(c + d + e + f + g.0 + h0 + h1a + h1b + h2 + i.1 .0) + a
     }
 
-    test_kernel_vm_and_verilog::<add<Red>, _, _, _>(add::<Red>, tuple_u8()).unwrap();
+    test_kernel_vm_and_verilog::<add<Red>, _, _, _>(add::<Red>, tuple_exhaustive_red())?;
+    Ok(())
 }
