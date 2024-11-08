0f631d7a31a14755225b5486ec64b09736ee7989
Samit Basu <basu.samit@gmail.com>
2024-11-08 08:06:58 -0800

    Clean up - need to finish the timing path.

--- rhdl-x/src/visit.rs
+++
@@ -1,8 +0,0 @@
-use anyhow::Result;
-use rhdl_core::Circuit;
-
-pub trait Visitor {
-    fn visit<C: Circuit>(&mut self, instance_name: &str, circuit: &C) -> Result<()>;
-    fn push(&mut self);
-    fn pop(&mut self);
-}
