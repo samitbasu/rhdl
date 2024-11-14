6397cbbaec4c8d23e20db73d9ec9a3b3ff987086
Samit Basu <basu.samit@gmail.com>
2024-11-13 22:23:30 -0800

    Add gray counter.  Need support for array circuits.

--- rhdl-core/src/circuit/circuit_impl.rs
+++ rhdl-core/src/circuit/circuit_impl.rs
@@ -1,6 +1,8 @@
 use crate::{
-    digital_fn::DigitalFn2, error::RHDLError, flow_graph::optimization::optimize_flow_graph,
-    DigitalFn, FlowGraph, Timed,
+    digital_fn::{DigitalFn2, NoKernel2},
+    error::RHDLError,
+    flow_graph::optimization::optimize_flow_graph,
+    trace_pop_path, trace_push_path, Digital, DigitalFn, FlowGraph, Timed,
 };
 
 use super::{circuit_descriptor::CircuitDescriptor, hdl_descriptor::HDLDescriptor};
@@ -41,5 +43,55 @@ pub trait Circuit: 'static + Sized + Clone + CircuitIO {
     fn flow_graph(&self, name: &str) -> Result<FlowGraph, RHDLError> {
         let flow_graph = self.descriptor(name)?.flow_graph.clone();
         optimize_flow_graph(flow_graph)
+    }
+}
+
+// Blanket implementation for an array of circuits.
+impl<T: CircuitIO, const N: usize> CircuitIO for [T; N] {
+    type I = [T::I; N];
+    type O = [T::O; N];
+    type Kernel = NoKernel2<Self::I, Self::Q, (Self::O, Self::D)>;
+}
+
+impl<T: CircuitIO, const N: usize> CircuitDQ for [T; N] {
+    type D = [T::I; N];
+    type Q = [T::O; N];
+}
+
+impl<T: Circuit, const N: usize> Circuit for [T; N] {
+    type S = [T::S; N];
+
+    fn init(&self) -> Self::S {
+        array_init::array_init(|i| self[i].init())
+    }
+
+    fn sim(&self, input: Self::I, state: &mut Self::S) -> Self::O {
+        trace_push_path("array");
+        let mut output = [T::O::init(); N];
+        for i in 0..N {
+            output[i] = self[i].sim(input[i], &mut state[i]);
+        }
+        trace_pop_path();
+        output
+    }
+
+    fn description(&self) -> String {
+        format!("array of {} x {}", N, self[0].description())
+    }
+
+    fn descriptor(&self, name: &str) -> Result<CircuitDescriptor, RHDLError> {
+        let mut descriptors = Vec::new();
+        for i in 0..N {
+            descriptors.push(self[i].descriptor(&format!("{}[{}]", name, i))?);
+        }
+        Ok(CircuitDescriptor::new(name, descriptors))
+    }
+
+    fn hdl(&self, name: &str) -> Result<HDLDescriptor, RHDLError> {
+        let mut descriptors = Vec::new();
+        for i in 0..N {
+            descriptors.push(self[i].hdl(&format!("{}[{}]", name, i))?);
+        }
+        Ok(HDLDescriptor::new(name, descriptors))
     }
 }
--- rhdl-fpga/src/cdc/mod.rs
+++ rhdl-fpga/src/cdc/mod.rs
@@ -1,1 +1,2 @@
+pub mod gray_counter;
 pub mod synchronizer;
