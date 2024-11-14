7fa759d52aa68131c5b84e4d3aedce1708121667
Samit Basu <basu.samit@gmail.com>
2024-11-13 22:23:30 -0800

    Add gray counter.  Need support for array circuits.

--- rhdl-core/src/types/timed.rs
+++ rhdl-core/src/types/timed.rs
@@ -15,3 +15,5 @@ impl<T0: Timed, T1: Timed> Timed for (T0, T1) {}
 impl<T0: Timed, T1: Timed, T2: Timed> Timed for (T0, T1, T2) {}
 
 impl<T0: Timed, T1: Timed, T2: Timed, T3: Timed> Timed for (T0, T1, T2, T3) {}
+
+impl<T: Timed, const N: usize> Timed for [T; N] {}
