522981b8938940845fd16c2c117b869669bd9248
Samit Basu <basu.samit@gmail.com>
2024-11-18 08:06:07 -0800

    WIP

--- rhdl-core/src/sim/testbench/mod.rs
+++ rhdl-core/src/sim/testbench/mod.rs
@@ -1,2 +1,27 @@
 pub mod asynchronous;
 pub mod synchronous;
+
+#[derive(Debug, Clone, Default)]
+pub struct TestBenchOptions {
+    pub vcd_file: Option<String>,
+    pub skip_first_cases: usize,
+    pub hold_time: u64,
+}
+
+impl TestBenchOptions {
+    fn vcd(self, vcd_file: &str) -> Self {
+        Self {
+            vcd_file: Some(vcd_file.into()),
+            ..self
+        }
+    }
+    fn skip(self, skip_first_cases: usize) -> Self {
+        Self {
+            skip_first_cases,
+            ..self
+        }
+    }
+    fn hold_time(self, hold_time: u64) -> Self {
+        Self { hold_time, ..self }
+    }
+}
