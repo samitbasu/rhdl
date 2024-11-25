d22d3237ca8e6687e4a0cda695b17fbb0dd4e1e6
Samit Basu <basu.samit@gmail.com>
2024-11-24 20:39:28 -0800

    Use Carloni buffers for AXI sender/receivers.

--- rhdl-fpga/src/axi4lite/channel/sender.rs
+++ rhdl-fpga/src/axi4lite/channel/sender.rs
@@ -1,22 +1,12 @@
 use rhdl::prelude::*;
 
-use crate::core::dff;
+use crate::{core::option::unpack, lid::option_carloni};
 
 use super::{ChannelMToS, ChannelSToM};
 
 #[derive(Clone, Debug, Synchronous, SynchronousDQ, Default)]
 pub struct U<T: Digital> {
-    // Register to hold the data
-    sample: dff::U<T>,
-    // Register to hold the valid signal
-    state: dff::U<State>,
-}
-
-#[derive(Copy, Clone, PartialEq, Debug, Digital, Default)]
-pub enum State {
-    #[default]
-    Idle,
-    Valid,
+    inner: option_carloni::U<T>,
 }
 
 #[derive(Copy, Clone, PartialEq, Debug, Digital)]
@@ -37,47 +27,23 @@ impl<T: Digital> SynchronousIO for U<T> {
     type Kernel = sender_kernel<T>;
 }
 
-#[kernel]
-pub fn unpack<T: Digital>(opt: Option<T>) -> (bool, T) {
-    match opt {
-        None => (false, T::init()),
-        Some(t) => (true, t),
-    }
-}
+//    i           inner           bus
+//  to_send -----> data ------> data/valid
+//        q        d   q        o
+//
+//    full <----- !ready <----- ready
+//
 
 #[kernel]
 pub fn sender_kernel<T: Digital>(cr: ClockReset, i: I<T>, q: Q<T>) -> (O<T>, D<T>) {
     let mut d = D::<T>::init();
     let mut o = O::<T>::init();
-    //  unpack the input
-    let (input_valid, input_data) = unpack::<T>(i.to_send);
-    // By default, the state will not change.
-    d.state = q.state;
-    match q.state {
-        State::Idle => {
-            if input_valid {
-                d.state = State::Valid;
-                d.sample = input_data;
-            }
-        }
-        State::Valid => {
-            if i.bus.ready {
-                if input_valid {
-                    d.state = State::Valid;
-                    d.sample = input_data;
-                } else {
-                    d.state = State::Idle;
-                    d.sample = T::init();
-                }
-            }
-        }
-    }
-    o.bus.data = q.sample;
-    o.bus.valid = q.state == State::Valid;
-    o.full = q.state != State::Idle;
-    // Reset logic
-    if cr.reset.any() {
-        o.full = false;
-    }
+    // Forward the to_send to the inner module
+    d.inner.data = i.to_send;
+    d.inner.ready = i.bus.ready;
+    let (is_valid, data) = unpack::<T>(q.inner.data);
+    o.bus.data = data;
+    o.bus.valid = is_valid;
+    o.full = !q.inner.ready;
     (o, d)
 }
