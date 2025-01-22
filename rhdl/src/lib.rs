2e843368664cd9b66dcf9be8f54ca3611aaad3e9
Samit Basu <basu.samit@gmail.com>
2025-01-22 11:24:49 -0800

    Move in FPGA crate.

--- rhdl/src/lib.rs
+++ rhdl/src/lib.rs
@@ -1,8 +1,10 @@
 pub mod prelude;
 pub mod rhdl_bits;
 pub mod rhdl_core;
+pub mod rhdl_fpga;
 pub mod rhdl_typenum;
 pub use rhdl_bits as bits;
 pub use rhdl_core as core;
+pub use rhdl_fpga as fpga;
 pub use rhdl_trace_type as rtt;
 pub use rhdl_typenum as typenum;
