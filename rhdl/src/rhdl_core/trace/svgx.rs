1463a9785b1f388b0ddd9c6ed9f3f54dc17b4e97
Samit Basu <basu.samit@gmail.com>
2025-02-13 20:45:09 -0800

    More work on trace generation.

--- rhdl/src/rhdl_core/trace/svgx.rs
+++ rhdl/src/rhdl_core/trace/svgx.rs
@@ -17,17 +17,17 @@ use crate::{
 //  A data region is defined by a start and end time and a string describing the value.
 //  A trace set is hierarchical.  So we can have a main trace contain data as well as subtraces.
 
-enum RegionTag {
-    Bool(bool),
-    Unsigned(u128),
-    Signed(i128),
-    String(String),
-}
-
 struct Region {
     start: u64,
     end: u64,
-    tag: Option<RegionTag>,
+    tag: Option<String>,
+    kind: RegionKind,
+}
+
+enum RegionKind {
+    True,
+    False,
+    Multibit,
 }
 
 struct TraceSet {
@@ -45,7 +45,90 @@ pub fn trace_out<T: Digital>(
     todo!()
 }
 
+fn build_time_trace<T: Digital>(data: &[(u64, T)], path: &Path) -> Vec<Region> {
+    slice_by_path_and_bucketize(data, path)
+        .iter()
+        .map(map_bucket_to_region)
+        .collect()
+}
+
+fn slice_by_path_and_bucketize<T: Digital>(data: &[(u64, T)], path: &Path) -> Vec<Bucket> {
+    let sliced = data
+        .iter()
+        .map(|(time, value)| (*time, value.typed_bits()))
+        .flat_map(|(time, tb)| tb.path(path).ok().map(|tb| (time, tb)));
+    bucketize(sliced)
+}
+
+fn path_slice<T: Digital>(data: &[(u64, T)], path: &Path) -> Vec<(u64, TypedBits)> {
+    data.iter()
+        .map(|(time, value)| (*time, value.typed_bits()))
+        .flat_map(|(time, tb)| tb.path(path).ok().map(|tb| (time, tb)))
+        .collect()
+}
+
+fn map_bucket_to_region(bucket: &Bucket) -> Region {
+    let kind = match bucket.data.bits.len() {
+        1 => match bucket.data.bits[0] {
+            BitX::Zero => RegionKind::False,
+            BitX::One => RegionKind::True,
+            _ => RegionKind::Multibit,
+        },
+        _ => RegionKind::Multibit,
+    };
+    Region {
+        start: bucket.start,
+        end: bucket.end,
+        tag: format_as_label(&bucket.data),
+        kind,
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct Bucket {
+    start: u64,
+    end: u64,
+    data: TypedBits,
+}
+
+fn bucketize(data: impl IntoIterator<Item = (u64, TypedBits)>) -> Vec<Bucket> {
+    let mut buckets = Vec::new();
+    let mut last_time = !0;
+    let mut last_data = TypedBits::EMPTY;
+    let mut start_time = !0;
+    for (time, data) in data.into_iter() {
+        if last_time == !0 {
+            last_time = time;
+            start_time = time;
+            last_data = data.clone();
+        } else {
+            if !last_data.eq(&data) {
+                buckets.push(Bucket {
+                    start: start_time,
+                    end: time,
+                    data: last_data.clone(),
+                });
+                start_time = time;
+                last_data = data.clone();
+            }
+            last_time = time;
+        }
+    }
+    if last_time != !0 {
+        buckets.push(Bucket {
+            start: start_time,
+            end: last_time,
+            data: last_data,
+        });
+    }
+    buckets
+}
+
 pub fn format_as_label(t: &TypedBits) -> Option<String> {
+    (t.bits.len() != 1).then(|| format_as_label_inner(t))?
+}
+
+fn format_as_label_inner(t: &TypedBits) -> Option<String> {
     match t.kind {
         Kind::Array(inner) => {
             let vals = (0..inner.size)
@@ -125,5 +208,85 @@ pub fn format_as_label(t: &TypedBits) -> Option<String> {
             Some(format!("{:?}@({})", color, val))
         }
         Kind::Empty => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::b8;
+
+    use super::*;
+
+    #[test]
+    fn test_bucket_empty() {
+        let data = [];
+        let buckets = bucketize(data);
+        assert_eq!(buckets.len(), 0);
+    }
+
+    #[test]
+    fn test_bucket_single() {
+        let data = [(0, b8(8).typed_bits())];
+        let buckets = bucketize(data);
+        assert_eq!(buckets.len(), 1);
+        assert_eq!(
+            buckets[0],
+            Bucket {
+                start: 0,
+                end: 0,
+                data: b8(8).typed_bits()
+            }
+        );
+    }
+
+    #[test]
+    fn test_buckets_no_transition() {
+        let n8 = b8(8).typed_bits();
+        let data = [(0, n8.clone()), (1, n8.clone()), (2, n8.clone())];
+        let buckets = bucketize(data);
+        assert_eq!(buckets.len(), 1);
+        assert_eq!(
+            buckets[0],
+            Bucket {
+                start: 0,
+                end: 2,
+                data: n8
+            }
+        );
+    }
+
+    #[test]
+    fn test_buckets() {
+        let data = [
+            (0, b8(8).typed_bits()),
+            (1, b8(4).typed_bits()),
+            (3, b8(5).typed_bits()),
+        ];
+        let buckets = bucketize(data);
+        assert_eq!(buckets.len(), 3);
+        assert_eq!(
+            buckets[0],
+            Bucket {
+                start: 0,
+                end: 1,
+                data: b8(8).typed_bits()
+            }
+        );
+        assert_eq!(
+            buckets[1],
+            Bucket {
+                start: 1,
+                end: 3,
+                data: b8(4).typed_bits()
+            }
+        );
+        assert_eq!(
+            buckets[2],
+            Bucket {
+                start: 3,
+                end: 3,
+                data: b8(5).typed_bits()
+            }
+        );
     }
 }
