4eac605d3098d525e0e8eeaec240f5c1e0bdb2b4
Samit Basu <basu.samit@gmail.com>
2025-08-17 09:19:35 -0700

    Add constructor functions.

--- rhdl-vlog/src/ast.rs
+++ rhdl-vlog/src/ast.rs
@@ -1,12 +1,23 @@
-use std::ops::RangeInclusive;
-
 #[derive(Clone, Debug)]
 pub struct ModuleList(pub Vec<ModuleDef>);
 
+pub fn module_list(modules: Vec<ModuleDef>) -> ModuleList {
+    ModuleList(modules)
+}
+
+use std::ops::RangeInclusive;
 #[derive(Clone, Debug)]
 pub enum HDLKind {
     Wire,
     Reg,
+}
+
+pub fn wire() -> HDLKind {
+    HDLKind::Wire
+}
+
+pub fn reg() -> HDLKind {
+    HDLKind::Reg
 }
 
 #[derive(Clone, Debug)]
@@ -16,10 +27,30 @@ pub enum Direction {
     Inout,
 }
 
+pub fn input() -> Direction {
+    Direction::Input
+}
+
+pub fn output() -> Direction {
+    Direction::Output
+}
+
+pub fn inout() -> Direction {
+    Direction::Inout
+}
+
 #[derive(Clone, Debug)]
 pub enum SignedWidth {
     Signed(RangeInclusive<u32>),
     Unsigned(RangeInclusive<u32>),
+}
+
+pub fn signed(range: RangeInclusive<u32>) -> SignedWidth {
+    SignedWidth::Signed(range)
+}
+
+pub fn unsigned(range: RangeInclusive<u32>) -> SignedWidth {
+    SignedWidth::Unsigned(range)
 }
 
 #[derive(Clone, Debug)]
@@ -29,10 +60,202 @@ pub struct Declaration {
     pub name: String,
 }
 
+pub fn declaration(kind: HDLKind, signed_width: SignedWidth, name: &str) -> Declaration {
+    Declaration {
+        kind,
+        signed_width,
+        name: name.to_string(),
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct Port {
     pub direction: Direction,
     pub decl: Declaration,
+}
+
+pub fn port(direction: Direction, decl: Declaration) -> Port {
+    Port { direction, decl }
+}
+
+#[derive(Debug, Clone)]
+pub struct Connection {
+    pub target: String,
+    pub local: Box<Expr>,
+}
+
+pub fn connection(target: &str, local: Expr) -> Connection {
+    Connection {
+        target: target.to_string(),
+        local: Box::new(local),
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Sensitivity {
+    PosEdge(String),
+    NegEdge(String),
+    Signal(String),
+    Star,
+}
+
+pub fn pos_edge(signal: &str) -> Sensitivity {
+    Sensitivity::PosEdge(signal.to_string())
+}
+
+pub fn neg_edge(signal: &str) -> Sensitivity {
+    Sensitivity::NegEdge(signal.to_string())
+}
+
+pub fn signal(signal: &str) -> Sensitivity {
+    Sensitivity::Signal(signal.to_string())
+}
+
+pub fn star() -> Sensitivity {
+    Sensitivity::Star
+}
+
+#[derive(Debug, Clone)]
+pub enum BinaryOp {
+    Shl,
+    SignedRightShift,
+    Shr,
+    ShortAnd,
+    ShortOr,
+    CaseEq,
+    CaseNe,
+    Ne,
+    Eq,
+    Ge,
+    Le,
+    Gt,
+    Lt,
+    Plus,
+    Minus,
+    And,
+    Or,
+    Xor,
+    Mod,
+    Mul,
+}
+
+pub fn binary_shl() -> BinaryOp {
+    BinaryOp::Shl
+}
+
+pub fn binary_signed_right_shift() -> BinaryOp {
+    BinaryOp::SignedRightShift
+}
+
+pub fn binary_shr() -> BinaryOp {
+    BinaryOp::Shr
+}
+
+pub fn binary_short_and() -> BinaryOp {
+    BinaryOp::ShortAnd
+}
+
+pub fn binary_short_or() -> BinaryOp {
+    BinaryOp::ShortOr
+}
+
+pub fn binary_case_eq() -> BinaryOp {
+    BinaryOp::CaseEq
+}
+
+pub fn binary_case_ne() -> BinaryOp {
+    BinaryOp::CaseNe
+}
+
+pub fn binary_ne() -> BinaryOp {
+    BinaryOp::Ne
+}
+
+pub fn binary_eq() -> BinaryOp {
+    BinaryOp::Eq
+}
+
+pub fn binary_ge() -> BinaryOp {
+    BinaryOp::Ge
+}
+
+pub fn binary_le() -> BinaryOp {
+    BinaryOp::Le
+}
+
+pub fn binary_gt() -> BinaryOp {
+    BinaryOp::Gt
+}
+
+pub fn binary_lt() -> BinaryOp {
+    BinaryOp::Lt
+}
+
+pub fn binary_plus() -> BinaryOp {
+    BinaryOp::Plus
+}
+
+pub fn binary_minus() -> BinaryOp {
+    BinaryOp::Minus
+}
+
+pub fn binary_and() -> BinaryOp {
+    BinaryOp::And
+}
+
+pub fn binary_or() -> BinaryOp {
+    BinaryOp::Or
+}
+
+pub fn binary_xor() -> BinaryOp {
+    BinaryOp::Xor
+}
+
+pub fn binary_mod() -> BinaryOp {
+    BinaryOp::Mod
+}
+
+pub fn binary_mul() -> BinaryOp {
+    BinaryOp::Mul
+}
+
+#[derive(Debug, Clone)]
+pub enum UnaryOp {
+    Plus,
+    Minus,
+    Bang,
+    Not,
+    And,
+    Or,
+    Xor,
+}
+
+pub fn unary_plus() -> UnaryOp {
+    UnaryOp::Plus
+}
+
+pub fn unary_minus() -> UnaryOp {
+    UnaryOp::Minus
+}
+
+pub fn unary_bang() -> UnaryOp {
+    UnaryOp::Bang
+}
+
+pub fn unary_not() -> UnaryOp {
+    UnaryOp::Not
+}
+
+pub fn unary_and() -> UnaryOp {
+    UnaryOp::And
+}
+
+pub fn unary_or() -> UnaryOp {
+    UnaryOp::Or
+}
+
+pub fn unary_xor() -> UnaryOp {
+    UnaryOp::Xor
 }
 
 #[derive(Default, Debug, Clone)]
@@ -56,6 +279,104 @@ pub enum Stmt {
     Noop,
 }
 
+pub fn if_stmt(condition: Expr, true_stmt: Stmt, else_branch: Option<Stmt>) -> Stmt {
+    Stmt::If(If {
+        condition: Box::new(condition),
+        true_stmt: Box::new(true_stmt),
+        else_branch: else_branch.map(Box::new),
+    })
+}
+
+pub fn always_stmt(sensitivity: Vec<Sensitivity>, body: Stmt) -> Stmt {
+    Stmt::Always(Always {
+        sensitivity,
+        body: Box::new(body),
+    })
+}
+
+pub fn case_stmt(discriminant: Expr, lines: Vec<CaseLine>) -> Stmt {
+    Stmt::Case(Case {
+        discriminant: Box::new(discriminant),
+        lines,
+    })
+}
+
+pub fn local_param_stmt(target: &str, rhs: LitVerilog) -> Stmt {
+    Stmt::LocalParam(LocalParam {
+        target: target.to_string(),
+        rhs,
+    })
+}
+
+pub fn block_stmt(stmts: Vec<Stmt>) -> Stmt {
+    Stmt::Block(stmts)
+}
+
+pub fn continuous_assign_stmt(target: &str, rhs: Expr) -> Stmt {
+    Stmt::ContinuousAssign(Assign {
+        target: target.to_string(),
+        rhs: Box::new(rhs),
+    })
+}
+
+pub fn function_call_stmt(name: &str, args: Vec<Expr>) -> Stmt {
+    Stmt::FunctionCall(FunctionCall {
+        name: name.to_string(),
+        args,
+    })
+}
+
+pub fn nonblock_assign_stmt(target: &str, rhs: Expr) -> Stmt {
+    Stmt::NonblockAssign(Assign {
+        target: target.to_string(),
+        rhs: Box::new(rhs),
+    })
+}
+
+pub fn assign_stmt(target: &str, rhs: Expr) -> Stmt {
+    Stmt::Assign(Assign {
+        target: target.to_string(),
+        rhs: Box::new(rhs),
+    })
+}
+
+pub fn instance_stmt(module: &str, instance: &str, connections: Vec<Connection>) -> Stmt {
+    Stmt::Instance(Instance {
+        module: module.to_string(),
+        instance: instance.to_string(),
+        connections,
+    })
+}
+
+pub fn splice_stmt(target: &str, msb: Expr, lsb: Option<Expr>, rhs: Expr) -> Stmt {
+    Stmt::Splice(Splice {
+        target: target.to_string(),
+        msb: Box::new(msb),
+        lsb: lsb.map(Box::new),
+        rhs: Box::new(rhs),
+    })
+}
+
+pub fn dynamic_splice_stmt(target: &str, base: Expr, width: Expr, rhs: Expr) -> Stmt {
+    Stmt::DynamicSplice(DynamicSplice {
+        target: target.to_string(),
+        base: Box::new(base),
+        width: Box::new(width),
+        rhs: Box::new(rhs),
+    })
+}
+
+pub fn delay_stmt(delay: u32) -> Stmt {
+    Stmt::Delay(delay)
+}
+
+pub fn concat_assign_stmt(target: Vec<Expr>, rhs: Expr) -> Stmt {
+    Stmt::ConcatAssign(ConcatAssign {
+        target,
+        rhs: Box::new(rhs),
+    })
+}
+
 #[derive(Debug, Clone)]
 pub struct ConcatAssign {
     pub target: Vec<Expr>,
@@ -76,12 +397,6 @@ pub struct Splice {
     pub msb: Box<Expr>,
     pub lsb: Option<Box<Expr>>,
     pub rhs: Box<Expr>,
-}
-
-#[derive(Debug, Clone)]
-pub struct Connection {
-    pub target: String,
-    pub local: Box<Expr>,
 }
 
 #[derive(Debug, Clone)]
@@ -109,17 +424,17 @@ pub struct LocalParam {
     pub rhs: LitVerilog,
 }
 
-#[derive(Debug, Clone, Copy)]
-pub enum BitX {
-    Zero,
-    One,
-    X,
-}
-
 #[derive(Debug, Clone)]
 pub struct LitVerilog {
     pub width: u32,
     pub value: String,
+}
+
+pub fn lit_verilog(width: u32, value: &str) -> LitVerilog {
+    LitVerilog {
+        width,
+        value: value.to_string(),
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -128,24 +443,31 @@ pub enum CaseItem {
     Wild,
 }
 
+pub fn case_item_literal(lit: LitVerilog) -> CaseItem {
+    CaseItem::Literal(lit)
+}
+
+pub fn case_item_wild() -> CaseItem {
+    CaseItem::Wild
+}
+
 #[derive(Debug, Clone)]
 pub struct CaseLine {
     pub item: CaseItem,
     pub stmt: Box<Stmt>,
 }
 
+pub fn case_line(item: CaseItem, stmt: Stmt) -> CaseLine {
+    CaseLine {
+        item,
+        stmt: Box::new(stmt),
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Case {
     pub discriminant: Box<Expr>,
     pub lines: Vec<CaseLine>,
-}
-
-#[derive(Debug, Clone)]
-pub enum Sensitivity {
-    PosEdge(String),
-    NegEdge(String),
-    Signal(String),
-    Star,
 }
 
 #[derive(Debug, Clone)]
@@ -169,6 +491,20 @@ pub struct FunctionDef {
     pub items: Vec<Item>,
 }
 
+pub fn function_def(
+    width: SignedWidth,
+    name: &str,
+    args: Vec<Port>,
+    items: Vec<Item>,
+) -> FunctionDef {
+    FunctionDef {
+        width,
+        name: name.to_string(),
+        args,
+        items,
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum Item {
     Statement(Stmt),
@@ -177,11 +513,35 @@ pub enum Item {
     Initial(Stmt),
 }
 
+pub fn stmt_item(stmt: Stmt) -> Item {
+    Item::Statement(stmt)
+}
+
+pub fn declaration_item(decl: Declaration) -> Item {
+    Item::Declaration(decl)
+}
+
+pub fn function_def_item(func: FunctionDef) -> Item {
+    Item::FunctionDef(func)
+}
+
+pub fn initial_item(initial: Stmt) -> Item {
+    Item::Initial(initial)
+}
+
 #[derive(Clone, Debug)]
 pub struct ModuleDef {
     pub name: String,
     pub args: Vec<Port>,
     pub items: Vec<Item>,
+}
+
+pub fn module_def(name: &str, args: Vec<Port>, items: Vec<Item>) -> ModuleDef {
+    ModuleDef {
+        name: name.to_string(),
+        args,
+        items,
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -199,6 +559,83 @@ pub enum Expr {
     Index(ExprIndex),
     DynIndex(ExprDynIndex),
     Function(ExprFunction),
+}
+
+pub fn binary_expr(lhs: Expr, op: BinaryOp, rhs: Expr) -> Expr {
+    Expr::Binary(ExprBinary {
+        lhs: Box::new(lhs),
+        op,
+        rhs: Box::new(rhs),
+    })
+}
+
+pub fn unary_expr(op: UnaryOp, arg: Expr) -> Expr {
+    Expr::Unary(ExprUnary {
+        op,
+        arg: Box::new(arg),
+    })
+}
+
+pub fn constant_expr(lit: LitVerilog) -> Expr {
+    Expr::Constant(lit)
+}
+
+pub fn literal_expr(value: i32) -> Expr {
+    Expr::Literal(value)
+}
+
+pub fn string_expr(value: &str) -> Expr {
+    Expr::String(value.to_string())
+}
+
+pub fn ident_expr(name: &str) -> Expr {
+    Expr::Ident(name.to_string())
+}
+
+pub fn paren_expr(expr: Expr) -> Expr {
+    Expr::Paren(Box::new(expr))
+}
+
+pub fn ternary_expr(condition: Expr, true_expr: Expr, false_expr: Expr) -> Expr {
+    Expr::Ternary(ExprTernary {
+        lhs: Box::new(condition),
+        mhs: Box::new(true_expr),
+        rhs: Box::new(false_expr),
+    })
+}
+
+pub fn concat_expr(exprs: Vec<Expr>) -> Expr {
+    Expr::Concat(exprs)
+}
+
+pub fn replica_expr(count: usize, concatenation: Vec<Expr>) -> Expr {
+    Expr::Replica(ExprReplica {
+        count,
+        concatenation,
+    })
+}
+
+pub fn index_expr(target: &str, msb: Expr, lsb: Option<Expr>) -> Expr {
+    Expr::Index(ExprIndex {
+        target: target.to_string(),
+        msb: Box::new(msb),
+        lsb: lsb.map(Box::new),
+    })
+}
+
+pub fn dyn_index_expr(target: &str, base: Expr, width: Expr) -> Expr {
+    Expr::DynIndex(ExprDynIndex {
+        target: target.to_string(),
+        base: Box::new(base),
+        width: Box::new(width),
+    })
+}
+
+pub fn function_expr(name: &str, args: Vec<Expr>) -> Expr {
+    Expr::Function(ExprFunction {
+        name: name.to_string(),
+        args,
+    })
 }
 
 #[derive(Debug, Clone)]
@@ -245,39 +682,4 @@ pub struct ExprReplica {
 pub struct ExprFunction {
     pub name: String,
     pub args: Vec<Expr>,
-}
-
-#[derive(Debug, Clone)]
-pub enum BinaryOp {
-    Shl,
-    SignedRightShift,
-    Shr,
-    ShortAnd,
-    ShortOr,
-    CaseEq,
-    CaseNe,
-    Ne,
-    Eq,
-    Ge,
-    Le,
-    Gt,
-    Lt,
-    Plus,
-    Minus,
-    And,
-    Or,
-    Xor,
-    Mod,
-    Mul,
-}
-
-#[derive(Debug, Clone)]
-pub enum UnaryOp {
-    Plus,
-    Minus,
-    Bang,
-    Not,
-    And,
-    Or,
-    Xor,
 }
