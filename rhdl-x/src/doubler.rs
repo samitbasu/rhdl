f2a0bde6437a785a2b4cf25e5354bb660af3e738
Samit Basu <basu.samit@gmail.com>
2025-08-25 20:50:38 -0700

    The great rearrangement

--- rhdl-x/src/doubler.rs
+++
@@ -1,9 +0,0 @@
-use rhdl::prelude::*;
-
-#[kernel]
-pub fn doubler<const N: usize>(_cr: ClockReset, i: Bits<N>) -> Bits<N> {
-    trace("i", &i);
-    let o = i << 1;
-    trace("o", &o);
-    i << 1
-}
