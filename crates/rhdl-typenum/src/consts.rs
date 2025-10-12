b083a054922dc5b69b77365c7f9e64bafbabad9f
Samit Basu <basu.samit@gmail.com>
2025-10-11 21:35:03 -0700

    typenum experiment...

--- crates/rhdl-typenum/src/consts.rs
+++ crates/rhdl-typenum/src/consts.rs
@@ -3,6 +3,7 @@ use seq_macro::seq;
 use super::prelude::Unsigned;
 
 seq!(N in 1..=128 {
+    /// Type-level unsigned integer constant.
     #[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug, Default)]
     pub struct U~N;
 
--- crates/rhdl-typenum/src/unsigned.rs
+++ crates/rhdl-typenum/src/unsigned.rs
@@ -1,5 +1,8 @@
+/// Trait for type-level unsigned integers.
 pub trait Unsigned: Copy + Default + 'static {
+    /// The value of the type-level integer as a `usize`.
     const USIZE: usize = 0;
+    /// Create a new instance of the type-level integer.
     fn new() -> Self {
         Self::default()
     }
